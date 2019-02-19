bffb4bcb22cd14d1113c1ac8ac6e8ae762fc6297
Vexatori <44672606+Vexatori@users.noreply.github.com>
2019-02-19 15:33:58 +0300

    IntMatrix.dll

---
+++ Matrix.lib/IntMatrixClass.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix.lib
+{
+    public class IntMatrixClass : ICloneable
+    {
+        private int _rows;
+        private int _columns;
+        private int[] _matrix;
+
+        public int Size { get => _rows; set => _rows = value; }
+        public int[] Matrix { get => _matrix; set => _matrix = value; }
+
+        public IntMatrixClass( int rows )
+        {
+            _rows = rows;
+            _columns = rows;
+            _matrix = new int[ _rows * _columns ];
+        }
+
+        public IntMatrixClass( int rows, int columns )
+        {
+            _rows = rows;
+            _columns = columns;
+            _matrix = new int[ _rows * _columns ];
+        }
+
+        public void SetRandomMatrix()
+        {
+            Random rnd = new Random();
+            for ( int i = 0; i < _columns * _rows; i++ ) { _matrix[ i ] = rnd.Next(); }
+        }
+
+        public void SetRandomMatrix( int min, int max )
+        {
+            Random rnd = new Random();
+            for ( int i = 0; i < _columns * _rows; i++ ) { _matrix[ i ] = rnd.Next( min, max + 1 ); }
+        }
+
+        public int GetElementAt( int i, int j ) => _matrix[ i * _columns + j ];
+
+        public object Clone() { return this.MemberwiseClone(); }
+    }
+}
---
+++ Matrix.lib/Utility.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix.lib
+{
+    public static class Utility
+    {
+        public static void Swap<T>( ref T a, ref T b )
+        {
+            var buf = a;
+            a = b;
+            b = buf;
+        }
+    }
+}
