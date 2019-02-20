2b5af598718a7fadeaee5c5c3f57f60e2ea4a5c4
Vexatori <44672606+Vexatori@users.noreply.github.com>
2019-02-20 10:48:12 +0300

    IntMatrix.lib updated

--- Matrix.lib/IntMatrixClass.cs
+++ Matrix.lib/IntMatrixClass.cs
@@ -13,7 +13,8 @@ namespace Matrix.lib
         private int _columns;
         private int[] _matrix;
 
-        public int Size { get => _rows; set => _rows = value; }
+        public int Rows { get => _rows; set => _rows = value; }
+        public int Columns { get => _columns; set => _columns = value; }
         public int[] Matrix { get => _matrix; set => _matrix = value; }
 
         public IntMatrixClass( int rows )
@@ -42,7 +43,86 @@ namespace Matrix.lib
             for ( int i = 0; i < _columns * _rows; i++ ) { _matrix[ i ] = rnd.Next( min, max + 1 ); }
         }
 
+        public void SetMatrixFromMatrix( int[ , ] matr )
+        {
+            _rows = matr.GetLength( 0 );
+            _columns = matr.GetLength( 1 );
+            for ( int i = 0; i < _rows; i++ )
+            {
+                for ( int j = 0; j < _columns; j++ ) { _matrix[ i * _columns + j ] = matr[ i, j ]; }
+            }
+        }
+
+        public void Show()
+        {
+            for ( int i = 0; i < _rows; i++ )
+            {
+                for ( int j = 0; j < _columns; j++ )
+                {
+                    Console.Write( $"{this.GetElementAt( i, j ), -5}" );
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        public IntMatrixClass SumMatrix( IntMatrixClass temp )
+        {
+            IntMatrixClass result = new IntMatrixClass( _rows, _columns );
+            for ( int i = 0; i < _columns * _rows; i++ )
+            {
+                result.SetElementAt( i, this.GetElementAt( i ) + temp.GetElementAt( i ) );
+            }
+
+            return result;
+        }
+
+        public IntMatrixClass MultiMatrix( IntMatrixClass temp )
+        {
+            IntMatrixClass result = new IntMatrixClass( _rows, temp.Columns );
+            for ( int i = 0; i < result.Rows; i++ )
+            {
+                for ( int j = 0; j < result.Columns; j++ )
+                {
+                    int someSum = SumOfProducts( this.GetRow( i ), temp.GetColumn( j ) );
+                    result.SetElementAt( i, j, someSum );
+                }
+            }
+
+            return result;
+        }
+
+        public int[] GetRow( int k )
+        {
+            int[] result = new int[ _columns ];
+            for ( int j = 0; j < _columns; j++ ) { result[ j ] = _matrix[ k * _columns + j ]; }
+
+            return result;
+        }
+
+        public int[] GetColumn( int k )
+        {
+            int[] result = new int[ _rows ];
+            for ( int i = 0; i < _rows; i++ ) { result[ i ] = _matrix[ i * _columns + k ]; }
+
+            return result;
+        }
+
+        private int SumOfProducts( int[] a, int[] b )
+        {
+            int result = 0;
+            for ( int i = 0; i < a.Length; i++ ) { result += a[ i ] * b[ i ]; }
+
+            return result;
+        }
+
         public int GetElementAt( int i, int j ) => _matrix[ i * _columns + j ];
+
+        public int GetElementAt( int i ) => _matrix[ i ];
+
+        public void SetElementAt( int i, int j, int value ) { _matrix[ i * _columns + j ] = value; }
+
+        public void SetElementAt( int i, int value ) { _matrix[ i ] = value; }
 
         public object Clone() { return this.MemberwiseClone(); }
     }
